64d87bc588ac8eb11f0a04991b484fde648c9b44
Scott Massey <scott.massey@wustl.edu>
2020-07-30 12:24:39 -0500

    added support for background audio on ios

--- scripts/install_ios.sh
+++ scripts/install_ios.sh
@@ -3,3 +3,16 @@ set -e
 set -x
 
 sed -i.bak 's/9.0/10.0/' ios/Podfile
+
+name="AdaloApp"
+
+
+
+if grep -q "<string>audio" ios/$name/Info.plist; then
+    echo "Background audio already supported, nothing to do here."
+elif grep -q UIBackgroundModes ios/$name/Info.plist
+then
+    plutil -insert UIBackgroundModes.0 -string 'audio' ios/$name/Info.plist
+else
+    plutil -insert UIBackgroundModes -xml "<array><string>audio</string></array>" ios/$name/Info.plist
+fi
