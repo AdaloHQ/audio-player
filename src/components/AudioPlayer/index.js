c930a5fe285717ec66f2b6679dc4c010fae64104
Kenneth Rangel <kennethrc@gmail.com>
2022-01-09 11:04:42 +0100

    refactor to functional component

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -1,42 +1,42 @@
-import React, { useRef, useState } from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
-import AudioPlayerSub from "./../AudioPlayer/AudioPlayer";
-import ControlScheme from "./../AudioPlayer/ControlScheme";
+import React, { useRef, useState } from 'react'
+import { Text, View, Image, StyleSheet } from 'react-native'
+import AudioPlayerSub from './../AudioPlayer/AudioPlayer'
+import ControlScheme from './../AudioPlayer/ControlScheme'
 
 Number.prototype.round = function (places) {
-  return +(Math.round(this + "e+" + places) + "e-" + places);
-};
+  return +(Math.round(this + 'e+' + places) + 'e-' + places)
+}
 
 const placeholderAlbumArt =
-  "https://img.buzzfeed.com/buzzfeed-static/static/2020-03/5/23/enhanced/25a67c968a0a/enhanced-262-1583449224-1.png?downsize=900:*&output-format=auto&output-quality=auto";
+  'https://img.buzzfeed.com/buzzfeed-static/static/2020-03/5/23/enhanced/25a67c968a0a/enhanced-262-1583449224-1.png?downsize=900:*&output-format=auto&output-quality=auto'
 
-export const AudioPlayer = (props) => {
-  const ref = useRef(null);
-  const { url, title, subtitle, artwork } = props;
+export function AudioPlayer(props) {
+  const ref = useRef(null)
+  const { url, title, subtitle, artwork } = props
   // Controls play/pause buttons, as well as playback
-  const [playing, setPlaying] = useState(false);
+  const [playing, setPlaying] = useState(false)
   // Validity of url being played
-  const [playable, setPlayable] = useState(true);
+  const [playable, setPlayable] = useState(true)
   // Proportion of song played (from 0 to 1)
-  const [progress, setProgress] = useState(0.0);
+  const [progress, setProgress] = useState(0.0)
 
   // Proportion of previous song played (0 if no previous song)
-  const [prevProgress, setPrevProgress] = useState(0.0);
+  const [prevProgress, setPrevProgress] = useState(0.0)
 
   // Duration of song
-  const [duration, setDuration] = useState(0.0);
+  const [duration, setDuration] = useState(0.0)
 
   // Time played of song (in seconds)
-  const [played, setPlayed] = useState(0.0);
+  const [played, setPlayed] = useState(0.0)
 
   const [track, setTrack] = useState({
     url,
     title,
     subtitle,
     artwork: artwork.artworkURL,
-  });
+  })
 
-  const [width, setWidth] = useState(null);
+  const [width, setWidth] = useState(null)
 
   const state = {
     playing,
@@ -47,7 +47,7 @@ export const AudioPlayer = (props) => {
     played,
     track,
     width,
-  };
+  }
 
   const {
     titleColor,
@@ -65,109 +65,109 @@ export const AudioPlayer = (props) => {
     topScreen,
     // whether to pause when changing away from audio player screens
     keepPlaying,
-  } = props;
+  } = props
 
   // Taken from range-slider code Jeremy wrote to dynamically change width
   const handleLayout = ({ nativeEvent }) => {
-    const { width: currentWidth } = (nativeEvent && nativeEvent.layout) || {};
+    const { width: currentWidth } = (nativeEvent && nativeEvent.layout) || {}
     if (currentWidth !== width) {
-      setWidth(currentWidth);
+      setWidth(currentWidth)
     }
-  };
+  }
 
   // Changes state to playing, and calls any additional actions
   const start = () => {
     const {
       playPauseButtons: { playAction },
-    } = props;
-    if (!playable) return;
+    } = props
+    if (!playable) return
     if (playAction) {
-      playAction(played.round(2), duration.round(2), progress.round(3));
+      playAction(played.round(2), duration.round(2), progress.round(3))
     }
-    setPlaying(true);
-  };
+    setPlaying(true)
+  }
 
   // Changes state to not playing, and calls any additional actions
   const pause = () => {
     const {
       playPauseButtons: { pauseAction },
-    } = props;
-    if (!playable) return;
+    } = props
+    if (!playable) return
     if (pauseAction) {
-      pauseAction(played.round(2), duration.round(2), progress.round(3));
+      pauseAction(played.round(2), duration.round(2), progress.round(3))
     }
-    setPlaying(false);
-  };
+    setPlaying(false)
+  }
 
   // Callback for rewind button. Rewind amount is preprogrammed by the customer
   const rewind = () => {
     const {
       forwardBackButtons: { backAction, backAmount },
-    } = props;
-    if (!playable) return;
+    } = props
+    if (!playable) return
     // Call additional actions
-    if (backAction) backAction();
+    if (backAction) backAction()
     // If rewinding would go negative or rewind is set to the beginning,
     // seek to 0. Else seek to the rewind
     if (played - backAmount > 0 && backAmount !== 0) {
-      const newProgress = (played - backAmount) / duration;
+      const newProgress = (played - backAmount) / duration
       // Uses refs to tell the audio player to seek
-      ref.current.seek(newProgress);
+      ref.current.seek(newProgress)
     } else {
-      ref.current.seek(0);
+      ref.current.seek(0)
     }
-  };
+  }
 
   // Similar logic to rewind, except for skipping forward
   const skip = () => {
     const {
       forwardBackButtons: { forwardAction, forwardAmount },
-    } = props;
-    if (!playable) return;
-    if (forwardAction) forwardAction();
+    } = props
+    if (!playable) return
+    if (forwardAction) forwardAction()
     if (played + forwardAmount < duration) {
-      const newProgress = (played + forwardAmount) / duration;
-      ref.current.seek(newProgress);
+      const newProgress = (played + forwardAmount) / duration
+      ref.current.seek(newProgress)
     } else {
-      ref.current.seek(1);
-      setPlaying(false);
+      ref.current.seek(1)
+      setPlaying(false)
     }
-  };
+  }
 
   // Generalized seek method. Used by progressbar to seek to any place
   const seek = (newProgress) => {
     // Uses refs to tell audio player to seek
-    ref.current.seek(newProgress);
+    ref.current.seek(newProgress)
     // Update progress and played
-    const newPlayed = duration * newProgress;
-    setPlayed(newPlayed);
-    setProgress(newProgress);
-  };
+    const newPlayed = duration * newProgress
+    setPlayed(newPlayed)
+    setProgress(newProgress)
+  }
 
   const updateDuration = (duration) => {
-    setDuration(duration);
-  };
+    setDuration(duration)
+  }
 
   const updatePlayed = (played) => {
-    setPlayed(played);
-  };
+    setPlayed(played)
+  }
 
   const updatePlaying = (bool) => {
-    setPlaying(bool);
-  };
+    setPlaying(bool)
+  }
   const updatePlayable = (bool) => {
-    setPlayable(bool);
-  };
+    setPlayable(bool)
+  }
   const updatePrevProgress = (newProgress) => {
-    setPrevProgress(newProgress);
-  };
+    setPrevProgress(newProgress)
+  }
 
   const endSong = () => {
-    const { endAction } = props;
-    if (endAction) endAction();
-  };
+    const { endAction } = props
+    if (endAction) endAction()
+  }
 
-  const artworkWidth = (width * artwork.artworkPercent) / 100;
+  const artworkWidth = (width * artwork.artworkPercent) / 100
 
   // noinspection JSSuspiciousNameCombination
   const dynamicStyles = {
@@ -175,37 +175,37 @@ export const AudioPlayer = (props) => {
       width: artworkWidth,
       height: artworkWidth,
       borderRadius: artwork.artworkRounding,
-      alignSelf: "center",
+      alignSelf: 'center',
     },
     title: {
-      color: titleColor ? titleColor : "#424242",
+      color: titleColor ? titleColor : '#424242',
       fontSize: titleSize ? titleSize : 16,
       padding: 4,
-      textAlign: "center",
-      fontWeight: "bold",
+      textAlign: 'center',
+      fontWeight: 'bold',
       paddingTop: 22,
     },
     subtitle: {
-      color: subtitleColor ? subtitleColor : "#424242",
+      color: subtitleColor ? subtitleColor : '#424242',
       fontSize: subtitleSize ? subtitleSize : 12,
       padding: 4,
-      textAlign: "center",
+      textAlign: 'center',
     },
-  };
+  }
 
   //custom fonts
   if (props.styles) {
     dynamicStyles.title = {
       ...dynamicStyles.title,
       ...props.styles.title,
-    };
+    }
     dynamicStyles.subtitle = {
       ...dynamicStyles.subtitle,
       ...props.styles.subtitle,
-    };
+    }
   } else if (_fonts) {
-    dynamicStyles.title.fontFamily = _fonts.body;
-    dynamicStyles.subtitle.fontFamily = _fonts.body;
+    dynamicStyles.title.fontFamily = _fonts.body
+    dynamicStyles.subtitle.fontFamily = _fonts.body
   }
 
   const buttonConfig = {
@@ -253,8 +253,8 @@ export const AudioPlayer = (props) => {
       onPress: leftRightButtons.rightAction,
       iconSize: leftRightButtons.iconSize,
     },
-  };
-  const showArtwork = artwork.showArtwork && artwork.enabled;
+  }
+  const showArtwork = artwork.showArtwork && artwork.enabled
   // A little redundant, but various if statements control for positioning
   // options chosen by customer.
 
@@ -282,19 +282,19 @@ export const AudioPlayer = (props) => {
       _fonts={_fonts}
       active={active}
       topScreen={topScreen}
-      keepPlaying={typeof keepPlaying !== "undefined" ? keepPlaying : true}
+      keepPlaying={typeof keepPlaying !== 'undefined' ? keepPlaying : true}
     />
-  );
+  )
 
-  const ControlSchemeComponent = <ControlScheme {...buttonConfig} {...state} />;
+  const ControlSchemeComponent = <ControlScheme {...buttonConfig} {...state} />
   const SongText = (
     <>
-      {title !== "" ? <Text style={dynamicStyles.title}>{title}</Text> : null}
-      {subtitle !== "" ? (
+      {title !== '' ? <Text style={dynamicStyles.title}>{title}</Text> : null}
+      {subtitle !== '' ? (
         <Text style={dynamicStyles.subtitle}>{subtitle}</Text>
       ) : null}
     </>
-  );
+  )
   return (
     <View onLayout={handleLayout}>
       <View style={styles.wrapper}>
@@ -306,13 +306,13 @@ export const AudioPlayer = (props) => {
           />
         )}
 
-        {progressBar.position === "aboveButtons" ? (
+        {progressBar.position === 'aboveButtons' ? (
           <>
             {SongText}
             {AudioPlayerComponent}
             {ControlSchemeComponent}
           </>
-        ) : progressBar.position === "aboveTitle" ? (
+        ) : progressBar.position === 'aboveTitle' ? (
           <>
             {AudioPlayerComponent}
             {SongText}
@@ -327,13 +327,13 @@ export const AudioPlayer = (props) => {
         )}
       </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   wrapper: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
