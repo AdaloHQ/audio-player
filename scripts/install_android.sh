b75296d18e6c0881358e2f2786c47c732ed18991
Zach Harris <zachbharris94@gmail.com>
2022-10-31 16:47:05 -0500

    update install script

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -4,6 +4,10 @@ set -x
 
 dir=$(dirname "${0}")
 
+sed -i.bak '/com.android.tools.build:gradle/a\
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30"
+' android/build.gradle
+
 sed -i.bak '/defaultConfig {/ a\
       multiDexEnabled true\
     ' android/app/build.gradle
