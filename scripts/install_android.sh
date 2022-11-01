4266bd05d246699009234e7f7b263c944420f672
Zach Harris <zachbharris94@gmail.com>
2022-11-01 13:48:41 -0500

    I suck at sed

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -8,8 +8,8 @@ sed -i.bak '/com.android.tools.build:gradle/a\
         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30"
 ' android/build.gradle
 
-sed -i.bak '' '$a\ org.gradle.jvm.version=11' android/build.gradle
-sed -i.bak '' '$a\ org.gradle.libraryelements="jar"' android/build.gradle
+sed -i.bak '$a\ org.gradle.jvm.version=11' android/build.gradle
+sed -i.bak '$a\ org.gradle.libraryelements="jar"' android/build.gradle
 
 sed -i.bak '/defaultConfig {/ a\
       multiDexEnabled true\
