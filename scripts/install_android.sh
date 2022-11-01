2ce839038b5d7d5583efe30081a5f5ea0e3057f4
Zach Harris <zachbharris94@gmail.com>
2022-11-01 16:25:39 -0500

    change gradle version

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -4,6 +4,8 @@ set -x
 
 dir=$(dirname "${0}")
 
+sed -i.bak 'com.android.tools.build:gradle:3.5.4/com.android.tools.build:gradle:4.2.2' android/build.gradle
+
 sed -i.bak '/defaultConfig {/ a\
       multiDexEnabled true\
     ' android/app/build.gradle
